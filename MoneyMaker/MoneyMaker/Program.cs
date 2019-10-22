17a5a6b183c35074c37b2d6c8b5d49a118ab8599
Matt Wright <matthewbwright@gmail.com>
2019-10-22 13:32:34 -0700

    Adds the logic for converting an amount into the smallest amount of Gold, Silver, and Bronze coins.

--- MoneyMaker/MoneyMaker/Program.cs
+++ MoneyMaker/MoneyMaker/Program.cs
@@ -11,7 +11,24 @@ namespace MoneyMaker
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Money Maker!");
+            Console.WriteLine("What is the amount you wish converted?");
+            string totalAsString = Console.ReadLine();
+            double totalAsDouble = Convert.ToDouble(totalAsString);
+            Console.WriteLine(totalAsString + " cents is equal to...");
 
+            int goldValue = 10;
+            int silverValue = 5;
+
+            double goldCoins = Math.Floor(totalAsDouble / goldValue);
+            double remainder = totalAsDouble % goldValue;
+
+            double silverCoins = Math.Floor(remainder / silverValue);
+
+            remainder = remainder % silverValue;
+
+            Console.WriteLine("Gold coins: " + goldCoins);
+            Console.WriteLine("Silver coins: " + silverCoins);
+            Console.WriteLine("Bronze coins: " + remainder);
         }
     }
 }
